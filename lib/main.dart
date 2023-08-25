beed6e1289418363573fe145f0e8b955b5dcc202
AhmedSalah1997 <108864201+AhmedSalah1997@users.noreply.github.com>
2023-08-26 00:24:41 +0300

    create note app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/mobile.dart';
-import 'package:note_app/web.dart';
+import 'package:note_app/home.dart';
+import 'package:note_app/resopnsive/mobile.dart';
+import 'package:note_app/resopnsive/web.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,27 +15,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Builder(
-          builder: (context) {
-            if (MediaQuery.of(context).size.width.toInt() <= 560)
-              return const MobileScreen();
-            return const WebScreen();
-          },
-        )
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
 
-        // LayoutBuilder(
-        //   builder: (context, constraints) {
-        //     debugPrint(constraints.minWidth.toInt().toString());
-        //     if (constraints.minWidth.toInt() <= 560) return const MobileScreen();
-        //     return const WebScreen();
-        //   },
-        // ),
-        );
+      // Builder(
+      //   builder: (context) {
+      //     if (MediaQuery.of(context).size.width.toInt() <= 560)
+      //       return  MediaQuery(
+      //         data: MediaQuery.of(context).copyWith(
+      //           textScaleFactor: 0.0,
+      //         ),
+      //         child: MobileScreen());
+      //     return const WebScreen();
+      //   },
+      // ),
+
+      // LayoutBuilder(
+      //   builder: (context, constraints) {
+      //     debugPrint(constraints.minWidth.toInt().toString());
+      //     if (constraints.minWidth.toInt() <= 560) return const MobileScreen();
+      //     return const WebScreen();
+      //   },
+      // ),
+    );
   }
 }
